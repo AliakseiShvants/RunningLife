2da265cd0c5f3f6efed8c3a79d615ce4cbe00918
Aliaksei Shvants <ashvants91@gmail.com>
2019-03-05 21:48:56 +0300

    add simple fragment and use saving fragment state

--- app/src/main/java/com/shvants/runninglife/MainActivity.java
+++ app/src/main/java/com/shvants/runninglife/MainActivity.java
@@ -2,7 +2,9 @@ package com.shvants.runninglife;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +16,15 @@ public class MainActivity extends AppCompatActivity {
         android.support.v7.widget.Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Toast.makeText(this, "Hello!", Toast.LENGTH_LONG).show();
+        Button button = findViewById(R.id.main_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .add(R.id.training_detail_fragment, new TrainingDetailFragment())
+                        .commit();
+            }
+        });
     }
 }
