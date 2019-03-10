9647291dc151689edf1a565f223e28a899010624
Aliaksei Shvants <ashvants91@gmail.com>
2019-03-10 17:29:48 +0300

    made status bar transparent

---
+++ app/src/main/java/com/shvants/runninglife/androidui/vk/VkProfileActivity.java
@@ -0,0 +1,39 @@
+package com.shvants.runninglife.androidui.vk;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+import com.shvants.runninglife.R;
+
+import java.util.Objects;
+
+public class VkProfileActivity extends AppCompatActivity {
+
+    public static final boolean TRUE = true;
+    public static final boolean FALSE = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vk_profile);
+
+//        View decorView = getWindow().getDecorView();
+//        int uiOptions = View.SYSTEM_UI_FLAG_LOW_PROFILE;
+//        decorView.setSystemUiVisibility(uiOptions);
+
+        Toolbar toolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
+
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(TRUE);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayShowTitleEnabled(FALSE);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+}
