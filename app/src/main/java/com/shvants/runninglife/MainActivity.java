6a165901ada66dc76d063306a8815e144936963d
AliakseiShvants <ashvants91@gmail.com>
2019-03-15 22:06:38 +0300

    added navigation view with listener

--- app/src/main/java/com/shvants/runninglife/MainActivity.java
+++ app/src/main/java/com/shvants/runninglife/MainActivity.java
@@ -1,24 +1,42 @@
 package com.shvants.runninglife;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.widget.Toast;
-
-import com.shvants.imgloader.Constants;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+        drawerLayout = findViewById(R.id.drawer_layout);
 
-        String hello = Constants.LIB_NAME;
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        drawerLayout.closeDrawers();
 
-        Toast.makeText(this, hello, Toast.LENGTH_LONG).show();
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        return true;
+                    }
+                });
+
+
     }
 }
