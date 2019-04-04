6a7a9c2fa8ebc439434726acb7cc1b9d1fe5efb9
Aliaksei Shvants <ashvants91@gmail.com>
2019-04-04 14:41:58 +0300

    add DrawerLayout and NavigationView in activity_main, add menu to action bar and naigation view

--- app/src/main/java/com/shvants/runninglife/MainActivity.java
+++ app/src/main/java/com/shvants/runninglife/MainActivity.java
@@ -2,8 +2,16 @@ package com.shvants.runninglife;
 
 import android.os.Bundle;
 
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.Objects;
+
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import static java.lang.Boolean.TRUE;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +22,12 @@ public class MainActivity extends AppCompatActivity {
 
         final Toolbar toolbar = findViewById(R.id.main_toolbar);
         setSupportActionBar(toolbar);
+
+        final ActionBar actionBar = getSupportActionBar();
+        Objects.requireNonNull(actionBar).setDisplayHomeAsUpEnabled(TRUE);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        final DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        final NavigationView navigationView = findViewById(R.id.nav_view);
     }
 }
